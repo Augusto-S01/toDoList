da4d174a263969b7a6fea4ccf3bbc24096531add
Augusto Santos <Augustosouzas01@gmail.com>
2023-05-17 17:27:36 -0300

    dockerizing project

--- todolist-react/src/services/subTaskService.ts
+++ todolist-react/src/services/subTaskService.ts
@@ -4,7 +4,7 @@ import { subTarefa } from '../models/subTarefa';
 
 export function patchSubTaskToDO(subTask:subTarefa,mainId:number):Promise<subTarefa> {
 
-    return axios.patch<subTarefa>('http://localhost:8080/subTask',{
+    return axios.patch<subTarefa>('http://127.0.0.1:8000/subTask',{
         idMain: mainId,
         id: subTask.id,
         status: "TO_DO"
@@ -17,7 +17,7 @@ export function patchSubTaskToDO(subTask:subTarefa,mainId:number):Promise<subTar
 }
 
 export function postSubTask(idMain:number,description:string,deadlineDate:Date):Promise<subTarefa> {
-    return axios.post<subTarefa>('http://localhost:8080/subTask',{
+    return axios.post<subTarefa>('http://127.0.0.1:8000/subTask',{
         idMain: idMain,
         description: description,
         deadlineDate: deadlineDate,
@@ -31,7 +31,7 @@ export function postSubTask(idMain:number,description:string,deadlineDate:Date):
 
 export function patchSubTaskInProgress(subTask:subTarefa,mainId:number):Promise<subTarefa> {
     
-        return axios.patch<subTarefa>('http://localhost:8080/subTask',{
+        return axios.patch<subTarefa>('http://127.0.0.1:8000/subTask',{
             idMain: mainId,
             id: subTask.id,
             status: "IN_PROGRESS"
@@ -45,7 +45,7 @@ export function patchSubTaskInProgress(subTask:subTarefa,mainId:number):Promise<
 
 export function patchSubTaskDone(subTask:subTarefa,mainId:number):Promise<subTarefa> {
         
-            return axios.patch<subTarefa>('http://localhost:8080/subTask',{
+            return axios.patch<subTarefa>('http://127.0.0.1:8000/subTask',{
                 idMain: mainId,
                 id: subTask.id,
                 status: "DONE"
@@ -58,7 +58,7 @@ export function patchSubTaskDone(subTask:subTarefa,mainId:number):Promise<subTar
 }
 
 export function deleteSubTask(subId:number,mainId:number) {
-    return axios.delete(`http://localhost:8080/subTask/${mainId}/${subId}`,{
+    return axios.delete(`http://127.0.0.1:8000/subTask/${mainId}/${subId}`,{
         headers: {
             'Access-Control-Allow-Origin': '*',
             "Access-Control-Allow-header": "Origin, X-Requested-With, Content-Type, Accept"
--- todolist-react/src/services/taskService.ts
+++ todolist-react/src/services/taskService.ts
@@ -2,22 +2,23 @@ import axios, { Axios } from "axios";
 import { Tarefa } from "../models/Tarefa";
 import SalvarTarefaDTO  from "../models/salvarTarefaDTO";
 
+
+
 export function getTasks(): Promise<Tarefa[]> {
 
-    return axios.get<Tarefa[]>('http://localhost:8080/task', {
+    return axios.get<Tarefa[]>('http://127.0.0.1:8000/task',{
         headers: {
             'Access-Control-Allow-Origin': '*',
             "Access-Control-Allow-header": "Origin, X-Requested-With, Content-Type, Accept"
-        }
-    }).then(response => response.data);
+    },
+    }
+    ).then(response => response.data);
 
-
-    
 }
 
 
 export function postTask(tarefaDto:SalvarTarefaDTO){
-    return axios.post<SalvarTarefaDTO>('http://localhost:8080/task',tarefaDto,{
+    return axios.post<SalvarTarefaDTO>('http://127.0.0.1:8000/task',tarefaDto,{
         headers: {
             'Access-Control-Allow-Origin': '*',
             "Access-Control-Allow-header": "Origin, X-Requested-With, Content-Type, Accept"
@@ -28,7 +29,7 @@ export function postTask(tarefaDto:SalvarTarefaDTO){
 
 export function patchToDo(tarefa:Tarefa):Promise<Tarefa> {
 
-    return axios.patch<Tarefa>('http://localhost:8080/task',{
+    return axios.patch<Tarefa>('http://127.0.0.1:8000/task',{
         id: tarefa.id,
         status: "TO_DO"
     },{
@@ -41,7 +42,7 @@ export function patchToDo(tarefa:Tarefa):Promise<Tarefa> {
 
 export function patchInProgress(tarefa:Tarefa):Promise<Tarefa> {
 
-    return axios.patch<Tarefa>('http://localhost:8080/task',{
+    return axios.patch<Tarefa>('http://127.0.0.1:8000/task',{
         id: tarefa.id,
         status: "IN_PROGRESS"
     },{
@@ -55,7 +56,7 @@ export function patchInProgress(tarefa:Tarefa):Promise<Tarefa> {
 
 export function patchDone(tarefa:Tarefa):Promise<Tarefa> {
 
-    return axios.patch<Tarefa>('http://localhost:8080/task',{
+    return axios.patch<Tarefa>('http://127.0.0.1:8000/task',{
         id: tarefa.id,
         status: "DONE"
     },{
@@ -67,7 +68,7 @@ export function patchDone(tarefa:Tarefa):Promise<Tarefa> {
 }
 
 export function atualizaTarefa(tarefaDto:SalvarTarefaDTO){
-    return axios.patch<SalvarTarefaDTO>('http://localhost:8080/task',tarefaDto,{
+    return axios.patch<SalvarTarefaDTO>('http://127.0.0.1:8000/task',tarefaDto,{
         headers: {
             'Access-Control-Allow-Origin': '*',
             "Access-Control-Allow-header": "Origin, X-Requested-With, Content-Type, Accept"
@@ -78,7 +79,7 @@ export function atualizaTarefa(tarefaDto:SalvarTarefaDTO){
 }
 
 export function deleteTask(id:number) {
-    return axios.delete(`http://localhost:8080/task/${id}`,{
+    return axios.delete(`http://127.0.0.1:8000/task/${id}`,{
         headers: {
             'Access-Control-Allow-Origin': '*',
             "Access-Control-Allow-header": "Origin, X-Requested-With, Content-Type, Accept"
